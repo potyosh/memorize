a73dce045aa146c969634adb0f09a9f52637f9d7
w239yuki <w239yuki@gmail.com>
2014-04-03 19:11:08 +0900

    select button action demo

---
+++ memorize/preferencesAppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  preferencesAppDelegate.h
+//  memorize
+//
+//  Created by yoshi on 2014/04/03.
+//  Copyright (c) 2014年 yoshi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface preferencesAppDelegate : NSObject
+- (IBAction)selectButton:(id)sender;
+
+@end
---
+++ memorize/preferencesAppDelegate.m
@@ -0,0 +1,16 @@
+//
+//  preferencesAppDelegate.m
+//  memorize
+//
+//  Created by yoshi on 2014/04/03.
+//  Copyright (c) 2014年 yoshi. All rights reserved.
+//
+
+#import "preferencesAppDelegate.h"
+
+@implementation preferencesAppDelegate
+
+- (IBAction)selectButton:(id)sender {
+    NSLog(@"aaa");
+}
+@end
